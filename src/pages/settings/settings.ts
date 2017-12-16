5eebaaed8cd1f43f708a0f56424374f669115590
yahya essam <y.essam@essraa.com>
2017-12-16 02:15:46 +0200

    Prepare for build

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 import { Storage } from '@ionic/storage';
 import { HomePage } from '../home/home';
 
-@IonicPage()
 @Component({
   selector: 'page-settings',
   templateUrl: 'settings.html',
@@ -26,7 +25,7 @@ export class SettingsPage {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad SettingsPage');
+
   }
   saveForm() {
     let location = {
